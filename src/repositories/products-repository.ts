7d094dcb9bf1cbc3ee7d3a62e616d6c7ebb37c0b
Alex <alex.96_2012@inbox.ru>
2023-06-25 21:10:36 +0900

    update name

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -1,26 +1,13 @@
 import { products } from "../products";
 
 export const productsRepository = {
-  // findProducts(title: string | null | undefined) {
-  //   if (title) {
-  //     return products.filter((p) => p.title.indexOf(title) > -1);
-  //   } else {
-  //     return products;
-  //   }
-  // },
-  // getProductById(id: number) {
-  //   const product = products.find((el) => el.id === id);
-  //   return product;
-  // },
-  getProductByTypeYarnAndProduct(typeProduct: any, typeYarn: any) {
-    const filerByProduct = products.filter(
-      (p) => p.typeProduct === typeProduct
-    );
+  getProductByTypeYarnAndName(typeName: any, typeYarn: any) {
+    const filerByProduct = products.filter((p) => p.typeName === typeName);
     return filerByProduct.filter((p) => p.typeYarn === typeYarn);
   },
-  getProductByTypeProduct(typeProduct: any) {
-    if (typeProduct > 0) {
-      return products.filter((p) => p.typeProduct === typeProduct);
+  getProductByTypeName(typeName: any) {
+    if (typeName > 0) {
+      return products.filter((p) => p.typeName === typeName);
     } else {
       return products;
     }
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -3,48 +3,25 @@ import { productsRepository } from "../repositories/products-repository";
 import { products } from "../products";
 export const productsRoute = Router({});
 
-// productsRoute.get("/", (req: Request, res: Response) => {
-//   const findProducts = productsRepository.findProducts(
-//     req.query.title?.toString()
-//   );
-//   res.send(findProducts);
-// });
-
-// productsRoute.get("/:id", (req: Request, res: Response) => {
-//   const product = productsRepository.getProductById(+req.params.id);
-//   if (product) {
-//     res.send(product);
-//   } else res.send(404);
-// });
-
-// productsRoute.get("/:typeProduct", (req: Request, res: Response) => {
-//   const product = productsRepository.getProductByTypeProduct(
-//     +req.params.typeProduct
-//   );
-//   if (product) {
-//     res.send(product);
-//   } else res.send(404);
-// });
-
 productsRoute.get("/", (req: Request, res: Response) => {
   let foundProduct = products;
-  const productTypeProduct = productsRepository.getProductByTypeProduct(
-    Number(req.query.typeProduct)
+  const productTypeName = productsRepository.getProductByTypeName(
+    Number(req.query.typeName)
   );
   const productTypeYarn = productsRepository.getProductByTypeYarn(
     Number(req.query.typeYarn)
   );
   console.log(req.query);
-  if (req.query.typeProduct && req.query.typeYarn) {
-    const productTypeYarnAndProduct =
-      productsRepository.getProductByTypeYarnAndProduct(
-        Number(req.query.typeProduct),
+  if (req.query.typeName && req.query.typeYarn) {
+    const productTypeYarnAndName =
+      productsRepository.getProductByTypeYarnAndName(
+        Number(req.query.typeName),
         Number(req.query.typeYarn)
       );
 
-    res.send(productTypeYarnAndProduct);
-  } else if (req.query.typeProduct) {
-    res.send(productTypeProduct);
+    res.send(productTypeYarnAndName);
+  } else if (req.query.typeName) {
+    res.send(productTypeName);
   } else if (req.query.typeYarn) {
     res.send(productTypeYarn);
   } else {
