789df53f7a33e1d37b076f322ed4ca0e76111002
Alex <alex.96_2012@inbox.ru>
2023-06-25 21:43:50 +0900

    add filter

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -1,18 +1,34 @@
 import { products } from "../products";
 
 export const productsRepository = {
-  getProductByTypeYarnAndName(typeName: any, typeYarn: any) {
+  getProductYarnNameSeason(typeName: number, typeYarn: number, season: number) {
+    const filerByName = products.filter((p) => p.typeName === typeName);
+    const filerByProductYarn = filerByName.filter(
+      (p) => p.typeYarn === typeYarn
+    );
+    return filerByProductYarn.filter((p) => p.season === season);
+  },
+  getProductNameSeason(typeName: number, season: number) {
+    const filerByName = products.filter((p) => p.typeName === typeName);
+    return filerByName.filter((p) => p.season === season);
+  },
+  getProductByTypeYarnAndSeason(typeYarn: number, season: number) {
+    const filerByYarn = products.filter((p) => p.typeYarn === typeYarn);
+    return filerByYarn.filter((p) => p.season === season);
+  },
+
+  getProductByTypeYarnAndName(typeName: number, typeYarn: number) {
     const filerByProduct = products.filter((p) => p.typeName === typeName);
     return filerByProduct.filter((p) => p.typeYarn === typeYarn);
   },
-  getProductByTypeName(typeName: any) {
+  getProductByTypeName(typeName: number) {
     if (typeName > 0) {
       return products.filter((p) => p.typeName === typeName);
     } else {
       return products;
     }
   },
-  getProductByTypeYarn(typeYarn: any) {
+  getProductByTypeYarn(typeYarn: number) {
     if (typeYarn > 0) {
       return products.filter((p) => p.typeYarn === typeYarn);
     } else {
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -4,21 +4,39 @@ import { products } from "../products";
 export const productsRoute = Router({});
 
 productsRoute.get("/", (req: Request, res: Response) => {
-  let foundProduct = products;
   const productTypeName = productsRepository.getProductByTypeName(
     Number(req.query.typeName)
   );
   const productTypeYarn = productsRepository.getProductByTypeYarn(
     Number(req.query.typeYarn)
   );
-  console.log(req.query);
-  if (req.query.typeName && req.query.typeYarn) {
+
+  if (req.query.typeName && req.query.typeYarn && req.query.season) {
+    const result = productsRepository.getProductYarnNameSeason(
+      Number(req.query.typeName),
+      Number(req.query.typeYarn),
+      Number(req.query.season)
+    );
+    res.send(result);
+  } else if (req.query.typeName && req.query.season) {
+    const result = productsRepository.getProductNameSeason(
+      Number(req.query.typeName),
+      Number(req.query.season)
+    );
+    res.send(result);
+  } else if (req.query.typeYarn && req.query.season) {
+    const productTypeYarnAndSeason =
+      productsRepository.getProductByTypeYarnAndSeason(
+        Number(req.query.typeYarn),
+        Number(req.query.season)
+      );
+    res.send(productTypeYarnAndSeason);
+  } else if (req.query.typeName && req.query.typeYarn) {
     const productTypeYarnAndName =
       productsRepository.getProductByTypeYarnAndName(
         Number(req.query.typeName),
         Number(req.query.typeYarn)
       );
-
     res.send(productTypeYarnAndName);
   } else if (req.query.typeName) {
     res.send(productTypeName);
