a0e9bd4e3ac7d84fabdd4231da2cc0681ad23f38
unknown <alex.96_2012@inbox.ru>
2024-01-20 10:18:17 +0900

    :fix

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,7 +1,6 @@
 import pkg from 'express';
 const { Router } = pkg;
 export const authRoute = Router({});
-
 import {  registration, login } from "../controllers/authController.js";
 authRoute.post("/login", login);
 authRoute.post("/registration",  registration);
